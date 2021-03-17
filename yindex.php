5082e2c02d01a9ef917300c52b6d7d5ff296cdbb
OtariOboladze <otari.oboladze@outlook.com>
2021-03-17 19:49:55 +0100

    backend platform start

---
+++ yindex.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+
+?>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario</title>
+</head>
+
+<body>
+    <form action="procesaFormulario.php" method="get">
+        <input type="text" name="nombre" placeholder="nombre">
+        <input type="text" name="telefono" placeholder="telefono">
+        <input type="text" name="edad" value="edad">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
