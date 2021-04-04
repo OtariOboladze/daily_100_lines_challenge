e5e8897aa7442f0e877c6215f34a4230a02e10fa
OtariOboladze <otari.oboladze@outlook.com>
2021-04-04 12:25:22 +0200

    start session

---
+++ php session/session.php
@@ -0,0 +1,38 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <form action="#" method="post">
+            <label for="">NIF</label>
+            <input type="text" name="nif" id="">
+            <label for="">Nombre</label>
+            <input type="text" name="nombre" id="">
+            <label for="">Direccion</label>
+            <input type="text" name="direccion" id="">
+            <input type="submit" name="alta" value="alta persona">
+            <span></span>
+        </form>
+        <br><br>
+        <table>
+            <tr>
+                <th>NIF</th>
+                <th>Nombre</th>
+                <th>Direccion</th>
+                <th></th>
+            </tr>
+        </table>
+        <br>
+        <form action="#" method="post">
+            <input type="submit" name="baja" value="baja personas">
+        </form>
+    </div>
+</body>
+
+</html>
