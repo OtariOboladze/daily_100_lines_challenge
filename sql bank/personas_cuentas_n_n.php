21ae2e2d207567696dc7080a3491e259bf883c83
OtariOboladze <otari.oboladze@outlook.com>
2021-04-26 19:49:46 +0200

    minor changes, discard previous one

--- sql bank/personas_cuentas_n_n.php
+++ sql bank/personas_cuentas_n_n.php
@@ -1,19 +1,22 @@
 <?php
 
-try {
-    //incorporar fichero de coneccion
-    require 'connection_bank_2.php';
+//header para caracteres castellano
+header('Content-Type: text/html; charset=UTF-8');
 
 //variables para sql insert
-$nif = 'ragferdfererfgtjme';
-$nombre = addslashes('rumfertgj·$%!"·%&/geee'); 
+$nif = 'ragdfjejme';
+$nombre = addslashes('rumfertg&/geee');
 $apellido = addslashes('hagidedasa');
 // $apellido = '); DROP TABLE personas #';  <-  code injection 
 $direccion = 'mdaaa';
 $telefono = '543345';
 $email = 'klde@mgelkua.com';
 
-    //sentencias SQL
+try {
+    //incorporar fichero de coneccion
+    require 'connection_bank_2.php';
+
+    //insert en la tabla de personas
     $sql = "INSERT INTO personas VALUES (NULL, '$nif', '$nombre','$apellido','$direccion','$telefono','$email', DEFAULT)";
 
     //ejecutar query
@@ -26,9 +29,40 @@ $email = 'klde@mgelkua.com';
     };
 
     //recuperar la clave primaria asignada
-    $id = mysqli_insert_id($connection_bank);
+    $idp = mysqli_insert_id($connection_bank);
 
-    echo "persona añadida corctamente con el id $id y emial $email";
+    //insert en la tabla de cuentas
+    $entidad = '0634';
+    $oficina = '0780';
+    $dc = '56';
+    $cuentas = '16345';
+    $saldo = 234;
+
+    $sql_2 = "INSERT INTO cuentas VALUES (NULL, '$entidad', '$oficina', '$dc', '$cuentas', '$saldo', DEFAULT)";
+
+    if (!mysqli_query($connection_bank, $sql_2)) {
+        if (mysqli_errno($connection_bank) == 1062) {
+            throw new Exception("Cuenta ya existe en base de datos");
+        } else {
+            throw new Exception(mysqli_error($connection_bank));
+        }
+    };
+
+    //recuperar la clave primaria asignado a la cuenta
+    $idc = mysqli_insert_id($connection_bank);
+
+    //insert en la tabla de relacion
+    $sql_3 = "INSERT INTO personas_cuentas VALUES ('$idp','$idc')";
+
+    if (!mysqli_query($connection_bank, $sql_3)) {
+        if (mysqli_errno($connection_bank) == 1062) {
+            throw new Exception("Relacion ya existe en base de datos");
+        } else {
+            throw new Exception(mysqli_error($connection_bank));
+        }
+    };
+
+    echo "persona añadida corctamente con el id $idp y cuenta $idc";
 } catch (Exception $e) {
     echo $e->getMessage();
 }
